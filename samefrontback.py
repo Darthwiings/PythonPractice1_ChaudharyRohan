e7dacc80b2228b7d0cdf7c858e7e57ea0e63c7fc
Darthwiings <105502133+Darthwiings@users.noreply.github.com>
2022-05-18 18:07:00 -0500

    Add files via upload

---
+++ samefrontback.py
@@ -0,0 +1,17 @@
+#given a list of integers, return True if the length of the list is greater than 1 AND
+#the first element and the last element are equal
+#complete the function as your answer
+def sameFrontBack(numbers):
+    if len(numbers) == 0:
+        return False
+    elif numbers[0] == numbers[-1] and len(numbers) > 1:
+        return True
+    else:
+        return False
+
+print(sameFrontBack([1,2,3]))              #should return False
+print(sameFrontBack([1,2,3,1]))            #should return True
+print(sameFrontBack([1,2,1]))              #should return True
+print(sameFrontBack([1,2,3,4,5,6,7]))      #should return False
+print(sameFrontBack([]))                   #should return False
+print(sameFrontBack([1]))                   #should return False
